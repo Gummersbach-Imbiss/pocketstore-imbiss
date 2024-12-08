c11f6762d59d9d8e14bf352f1e3ef13597ba9cee
Jonathan Martz <git@jonathan-martz.de>
2024-12-08 02:02:48 +0100

    start adding image and content for index layout default and header

---
+++ custom/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="header bg-green-800 px-3 py-3 mb-3">
+    <section class="flex justify-between">
+      <a href="/" class="text-white font-bold text-xl">
+        Gummersbach-Imbiss.de
+      </a>
+    </section>
+  </section>
+</template>
---
+++ custom/pages/index.vue
@@ -0,0 +1,226 @@
+<script setup>
+import { useBreadcrumbStore } from '~/stores/breadcrumb';
+
+const storeBreadcrumb = useBreadcrumbStore();
+storeBreadcrumb.clear();
+</script>
+
+<template>
+    <section class="page">
+        <div class="max-w-6xl grid grid-cols-6 gap-3 px-3 py-3 bg-white rounded-lg mx-3 xl:mx-auto">
+            <div class="col-span-6">
+                <div class="bg-cover h-48 bg-cover" style="background-image:url('/luca-bravo-8x_fFNrmeDk-unsplash.jpg')"></div>
+            </div>
+            <div class="col-span-6 md:col-span-3">
+                <a href="/" class="btn btn-primary btn-block">Kategorie 1</a>
+                <div class="overflow-x-auto">
+                    <table class="table table-zebra">
+                        <!-- head -->
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- row 1 -->
+                            <tr>
+                                <th>1</th>
+                                <td>Cy Ganderton</td>
+                                <td>Blue</td>
+                            </tr>
+                            <!-- row 2 -->
+                            <tr>
+                                <th>2</th>
+                                <td>Hart Hagerty</td>
+                                <td>Purple</td>
+                            </tr>
+                            <!-- row 3 -->
+                            <tr>
+                                <th>3</th>
+                                <td>Tax Accountant</td>
+                                <td>Red</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col-span-6 md:col-span-3">
+                <a href="/" class="btn btn-primary btn-block">Kategorie 1</a>
+                <div class="overflow-x-auto">
+                    <table class="table table-zebra">
+                        <!-- head -->
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- row 1 -->
+                            <tr>
+                                <th>1</th>
+                                <td>Cy Ganderton</td>
+                                <td>Blue</td>
+                            </tr>
+                            <!-- row 2 -->
+                            <tr>
+                                <th>2</th>
+                                <td>Hart Hagerty</td>
+                                <td>Purple</td>
+                            </tr>
+                            <!-- row 3 -->
+                            <tr>
+                                <th>3</th>
+                                <td>Tax Accountant</td>
+                                <td>Red</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col-span-6 md:col-span-3">
+                <a href="/" class="btn btn-primary btn-block">Kategorie 1</a>
+                <div class="overflow-x-auto">
+                    <table class="table table-zebra">
+                        <!-- head -->
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- row 1 -->
+                            <tr>
+                                <th>1</th>
+                                <td>Cy Ganderton</td>
+                                <td>Blue</td>
+                            </tr>
+                            <!-- row 2 -->
+                            <tr>
+                                <th>2</th>
+                                <td>Hart Hagerty</td>
+                                <td>Purple</td>
+                            </tr>
+                            <!-- row 3 -->
+                            <tr>
+                                <th>3</th>
+                                <td>Tax Accountant</td>
+                                <td>Red</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col-span-6 md:col-span-3">
+                <a href="/" class="btn btn-primary btn-block">Kategorie 1</a>
+                <div class="overflow-x-auto">
+                    <table class="table table-zebra">
+                        <!-- head -->
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- row 1 -->
+                            <tr>
+                                <th>1</th>
+                                <td>Cy Ganderton</td>
+                                <td>Blue</td>
+                            </tr>
+                            <!-- row 2 -->
+                            <tr>
+                                <th>2</th>
+                                <td>Hart Hagerty</td>
+                                <td>Purple</td>
+                            </tr>
+                            <!-- row 3 -->
+                            <tr>
+                                <th>3</th>
+                                <td>Tax Accountant</td>
+                                <td>Red</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col-span-6 md:col-span-3">
+                <a href="/" class="btn btn-primary btn-block">Kategorie 1</a>
+                <div class="overflow-x-auto">
+                    <table class="table table-zebra">
+                        <!-- head -->
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- row 1 -->
+                            <tr>
+                                <th>1</th>
+                                <td>Cy Ganderton</td>
+                                <td>Blue</td>
+                            </tr>
+                            <!-- row 2 -->
+                            <tr>
+                                <th>2</th>
+                                <td>Hart Hagerty</td>
+                                <td>Purple</td>
+                            </tr>
+                            <!-- row 3 -->
+                            <tr>
+                                <th>3</th>
+                                <td>Tax Accountant</td>
+                                <td>Red</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col-span-6 md:col-span-3">
+                <a href="/" class="btn btn-primary btn-block">Kategorie 1</a>
+                <div class="overflow-x-auto">
+                    <table class="table table-zebra">
+                        <!-- head -->
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- row 1 -->
+                            <tr>
+                                <th>1</th>
+                                <td>Cy Ganderton</td>
+                                <td>Blue</td>
+                            </tr>
+                            <!-- row 2 -->
+                            <tr>
+                                <th>2</th>
+                                <td>Hart Hagerty</td>
+                                <td>Purple</td>
+                            </tr>
+                            <!-- row 3 -->
+                            <tr>
+                                <th>3</th>
+                                <td>Tax Accountant</td>
+                                <td>Red</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
